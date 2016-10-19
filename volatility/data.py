d100f8266b3c0d992b6163c8ad80986b3867b4d1
Jason <strimp101@gmail.com>
2016-10-19 11:29:26 +0800

    Updated to use pandas_datareader

--- volatility/data.py
+++ volatility/data.py
@@ -1,27 +1,38 @@
 import pandas
-import pandas.io.data as pd
+from pandas_datareader.yahoo.daily import YahooDailyReader
 
-def get_data(ticker, start, end):
 
-    data = pd.DataReader(ticker, "yahoo", start, end)
-
-    adj_factor = data['Adj Close'] / data['Close']
-
-    adj_open = data['Open'] * adj_factor
-    adj_high = data['High'] * adj_factor
-    adj_low = data['Low'] * adj_factor
+def get_data(symbols, start, end, adjust_price=True, interval='d'):
+    """
+    Returns DataFrame/Panel of historical stock prices from symbols, over date
+    range, start to end. 
     
-    returns = (data['Adj Close'] / data['Adj Close'].shift(1)) - 1
+    Parameters
+        ----------
+        symbols : string, array-like object (list, tuple, Series), or DataFrame
+            Single stock symbol (ticker), array-like object of symbols or
+            DataFrame with index containing stock symbols.
+        start : string, (defaults to '1/1/2010')
+            Starting date, timestamp. Parses many different kind of date
+            representations (e.g., 'JAN-01-2010', '1/1/10', 'Jan, 1, 1980')
+        end : string, (defaults to today)
+            Ending date, timestamp. Same format as starting date.
+    
+        adjust_price : bool, default False
+            If True, adjusts all prices in hist_data ('Open', 'High', 'Low',
+            'Close') based on 'Adj Close' price. Adds 'Adj_Ratio' column and drops
+            'Adj Close'.
 
-    return pandas.DataFrame({
-        'Open': data['Open'],
-        'High': data['High'], 
-        'Low': data['Low'],
-        'Close': data['Close'],
-        'Volume': data['Volume'],
-        'Adj Open': adj_open,
-        'Adj High': adj_high,
-        'Adj Low': adj_low,
-        'Adj Close': data['Adj Close'],
-        'Return': returns
-    }, index=data.index)
+        interval : string, default 'd'
+            Time interval code, valid values are 'd' for daily, 'w' for weekly,
+            'm' for monthly and 'v' for dividend.
+    
+    """
+    
+    data = YahooDailyReader(symbols=symbols, start=start, end=end, 
+                            adjust_price=adjust_price, 
+                            interval=interval).read()
+    del data['Adj_Ratio']
+    data['Return'] = (data['Close'] / data['Close'].shift(1)) - 1
+
+    return data
