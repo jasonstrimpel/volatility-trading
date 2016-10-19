cc60d91da031135fec585985c6553f242688c1ea
Jason <strimp101@gmail.com>
2016-10-19 13:09:56 +0800

    Fix model, close BUG#2

--- volatility/models/GarmanKlass.py
+++ volatility/models/GarmanKlass.py
@@ -12,10 +12,7 @@ def get_estimator(symbol, start, end, window=30, clean=True):
     
     log_hl = (prices['High'] / prices['Low']).apply(np.log)
     log_co = (prices['Close'] / prices['Open']).apply(np.log)
-    #log_oc = (prices['Open'] / prices['Close']).apply(np.log)
-    
-    #rs = log_oc**2 + 0.5 * log_hl**2 - 0.3862 * log_co**2
-    
+
     rs = 0.5 * log_hl**2 - (2*math.log(2)-1) * log_co**2
     
     def f(v):
