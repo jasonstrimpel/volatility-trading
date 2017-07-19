48f4ea3932572555374b4e51658bd0a076045c9f
Jason <strimp101@gmail.com>
2017-07-19 13:39:38 +0800

    Removed internal data acquisition

--- volatility/models/YangZhang.py
+++ volatility/models/YangZhang.py
@@ -2,32 +2,35 @@ import math
 
 import numpy as np
 
-import data
 
+def get_estimator(price_data, window=30, clean=True):
 
-def get_estimator(symbol, start, end, window=30, clean=True):
+    log_ho = (price_data['High'] / price_data['Open']).apply(np.log)
+    log_lo = (price_data['Low'] / price_data['Open']).apply(np.log)
+    log_co = (price_data['Close'] / price_data['Open']).apply(np.log)
     
-    prices = data.get_data(symbol, start, end)
-    
-    log_ho = (prices['High'] / prices['Open']).apply(np.log)
-    log_lo = (prices['Low'] / prices['Open']).apply(np.log)
-    log_co = (prices['Close'] / prices['Open']).apply(np.log)
-    
-    log_oc = (prices['Open'] / prices['Close'].shift(1)).apply(np.log)
+    log_oc = (price_data['Open'] / price_data['Close'].shift(1)).apply(np.log)
     log_oc_sq = log_oc**2
     
-    log_cc = (prices['Close'] / prices['Close'].shift(1)).apply(np.log)
+    log_cc = (price_data['Close'] / price_data['Close'].shift(1)).apply(np.log)
     log_cc_sq = log_cc**2
     
     rs = log_ho * (log_ho - log_co) + log_lo * (log_lo - log_co)
     
-    close_vol = log_cc_sq.rolling(window=window, center=False).sum() * (1.0 / (window - 1.0))
-    open_vol = log_oc_sq.rolling(window=window, center=False).sum() * (1.0 / (window - 1.0))
-    window_rs = rs.rolling(window=window, center=False).sum() * (1.0 / (window - 1.0))
+    close_vol = log_cc_sq.rolling(
+        window=window,
+        center=False
+    ).sum() * (1.0 / (window - 1.0))
+    open_vol = log_oc_sq.rolling(
+        window=window,
+        center=False
+    ).sum() * (1.0 / (window - 1.0))
+    window_rs = rs.rolling(
+        window=window,
+        center=False
+    ).sum() * (1.0 / (window - 1.0))
     
     result = (open_vol + 0.164333 * close_vol + 0.835667 * window_rs).apply(np.sqrt) * math.sqrt(252)
-    
-    result[:window-1] = np.nan
 
     if clean:
         return result.dropna()
