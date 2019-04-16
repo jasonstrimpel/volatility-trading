761b0d05e98a70ce2a08179dfebd5f64ad9f1ccb
Jason <jasonstrimpel@users.noreply.github.com>
2019-04-16 14:39:20 +0800

    Corrected the calculatrion which solves issue #11

--- volatility/models/GarmanKlass.py
+++ volatility/models/GarmanKlass.py
@@ -6,9 +6,9 @@ import numpy as np
 def get_estimator(price_data, window=30, trading_periods=252, clean=True):
 
     log_hl = (price_data['High'] / price_data['Low']).apply(np.log)
-    log_cc = (price_data['Close'] / price_data['Close'].shift(1)).apply(np.log)
+    log_co = (price_data['Close'] / price_data['Open']).apply(np.log)
 
-    rs = 0.5 * log_hl**2 - (2*math.log(2)-1) * log_cc**2
+    rs = 0.5 * log_hl**2 - (2*math.log(2)-1) * log_co**2
     
     def f(v):
         return (trading_periods * v.mean())**0.5
