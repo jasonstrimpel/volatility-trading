751b8f123688d0bbfe5441525bf6ec9d637b7df4
occasionallydavid <62972099+occasionallydavid@users.noreply.github.com>
2020-09-27 23:40:40 +0100

    Minimal setup.py
    Permits pip install via git+ssh://... references and similar.

---
+++ setup.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+from setuptools import setup
+
+setup(
+    name='volatility-trading',
+    version='0.0.1',
+    url='https://github.com/jasonstrimpel/volatility-trading/',
+    packages=['volatility'],
+)
