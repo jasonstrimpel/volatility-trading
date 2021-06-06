ecbe841e5aec98be6f3154cd21c950b09dcf09a7
AmirMaleki <maleki7425@gmail.com>
2021-06-06 15:49:53 +0430

    Corrected formulas for Parkinson and RogersSatchell

--- volatility/models/RogersSatchell.py
+++ volatility/models/RogersSatchell.py
@@ -12,7 +12,7 @@ def get_estimator(price_data, window=30, trading_periods=252, clean=True):
     rs = log_ho * (log_ho - log_co) + log_lo * (log_lo - log_co)
 
     def f(v):
-        return trading_periods * v.mean()**0.5
+        return (trading_periods * v.mean())**0.5
     
     result = rs.rolling(
         window=window,
